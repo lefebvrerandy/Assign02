5db6983feb68bacfe130a7875974ed97c812ed75
Randy Lefebvre <Rlefebvre2256@conestogac.on.ca>
2018-11-22 14:53:34 -0500

    Initial commit. Project started. Empty class for Triangle. Empty TestClass.

---
+++ Triangle.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Triangle.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Triangle.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("144f7ce7-fa71-46fc-ad06-30890ca3cfed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Triangle.Tests/TriangleTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
